7c561eada5991b68c0635763c1e98691821a46f2
Gr1cLev <richiegiansanto@gmail.com>
2025-11-25 11:26:41 +0700

    Update ChatBotWrapper.js

--- components/ChatBotWrapper.js
+++ components/ChatBotWrapper.js
@@ -1,15 +1,22 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ChatBot from './ChatBot';
 
 export default function ChatBotWrapper() {
   const [isChatbotOpen, setIsChatbotOpen] = useState(false);
 
-  // Fungsi ini akan dipanggil dari Header
-  if (typeof window !== 'undefined') {
+  useEffect(() => {
+    // Fungsi ini akan dipanggil dari Header
     window.openChatbot = () => setIsChatbotOpen(true);
-  }
+    
+    return () => {
+      // Cleanup
+      if (window.openChatbot) {
+        delete window.openChatbot;
+      }
+    };
+  }, []);
 
   return (
     <ChatBot 
